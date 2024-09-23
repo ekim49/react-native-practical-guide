3c38e79e3ab1dea762450394c76ff2cf83b15a38
Kelly Kim <kelly@madsq.net>
2024-09-23 21:58:45 +0900

    feat: 이미지 및 모달 추가

---
+++ components/GoalInput.js
@@ -0,0 +1,88 @@
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image,
+} from 'react-native';
+import { useState } from 'react';
+
+const GoalInput = (props) => {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText('');
+  };
+
+  return (
+    <Modal
+      visible={props.visible}
+      animationType="slide"
+    >
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/image/goal.png')}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title="Add Goal"
+              onPress={addGoalHandler}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Cancel"
+              onPress={props.onCancel}
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    // flexDirection: 'column ', // default
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#311b6b',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '100%',
+    padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+});
